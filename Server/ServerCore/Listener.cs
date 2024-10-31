8585e91abade8fb5a3d1529e008641c703fdd517
minsour7 <rlaalstn1677@naver.com>
2024-11-01 00:56:44 +0900

    초기 커밋

---
+++ Server/ServerCore/Listener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listnSocket;
+        Action<Socket> _onAcceptHandler;
+        public void init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listnSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            // 문지기 교육
+            _listnSocket.Bind(endPoint);
+
+            // 영업 시작
+            // backing : 최대 대기수
+            _listnSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            bool pending = _listnSocket.AcceptAsync(args);
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            RegisterAccept(args);
+        }
+    }
+}
