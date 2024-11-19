342fd0ea6f46afa8df5babfefe3173a4863c8206
minsour7 <rlaalstn143@gmail.com>
2024-11-19 22:28:14 +0900

    Connect Count Change

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -18,7 +18,7 @@ namespace DummyClient
             Connector connector = new Connector();
 
             connector.Connect(endPoint,
-                () => { return SessionManager.Instance.Generate(); }, 100);
+                () => { return SessionManager.Instance.Generate(); }, 500);
 
             while (true)
             {
