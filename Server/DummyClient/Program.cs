b53cb51a4a2321fcf783145f812d3a440e732f0a
minsour7 <rlaalstn143@gmail.com>
2024-11-20 23:10:02 +0900

    Save

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -9,30 +9,7 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            // DNS
-            string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
-
-            Connector connector = new Connector();
-
-            connector.Connect(endPoint,
-                () => { return SessionManager.Instance.Generate(); }, 500);
-
-            while (true)
-            {
-                try
-                {
-                    SessionManager.Instance.SendForEach();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-
-                Thread.Sleep(250);
-            }
+     
         }
     }
 }
